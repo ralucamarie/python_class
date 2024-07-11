f86903c2ed7d59854db158ee2661ba84c4da9b62
raluca.marie <raluca.marie@p4b.ro>
2024-07-11 05:49:09 +0300

    instructiuni and calculator

---
+++ 02_datastructures/liste.py
@@ -0,0 +1,33 @@
+# lista = []
+# print(type(lista))
+# lista = [8, 2, 'Ana are mere', 5, 'pere', 34, 89, 'struguri', False]
+# print(lista[2])
+# print(lista[-1])
+# print(lista[0:3])
+# print(lista[0:8])
+# print(lista[0:5:2])
+# print(lista[2:])
+# print(lista[:4])
+# print(len(lista))
+# print(lista.index(20))
+# print(lista.index(89))
+# lista.append('pepene rosu')
+# print(lista.index('pepene rosu'))
+# lista.insert(2, 'pepene')
+# print(lista)
+# lista.remove(34)
+# lista.remove(20)
+# lista.pop()
+# lista.pop(2)
+# print(lista.count(20))
+# lista.clear()
+# lista.reverse()
+lista = [8, 2, 'Ana are mere', 5, 'pere', 34, 89, 'struguri', False]
+lista2 = [1, 23, 55]
+# lista_noua = lista + lista2
+# lista_noua = lista.extend(lista2)
+print(lista)
+
+
+
+
